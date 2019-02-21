cc4c594d7f0dc98726a3f07a477a65c5dc429c32
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-21 15:48:09 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,8 +15,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price
       t.string :size
       t.string :image_url
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.timestamp :created_at
+      t.timestamp :updated_at
     end
   end
 end
